f175db8bbe354fde1d4d5c9d9e9bb6bd3f3ce242
rodrigopozodev <rodrigopozosanchez@gmail.com>
2025-04-02 23:03:09 +0200

    creacion de componentes home y save

---
+++ frontend/src/app/pages/save/save.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-save',
+  imports: [],
+  templateUrl: './save.component.html',
+  styleUrl: './save.component.css'
+})
+export class SaveComponent {
+
+}
