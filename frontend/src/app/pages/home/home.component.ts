1e0a326c1f3a6fd1d62052ef68c3d97509c3fede
rodrigopozodev <rodrigopozosanchez@gmail.com>
2025-04-02 23:13:17 +0200

    Actualizamos el componente Home para usar el servicio
    Agregamos la barra de progreso al template

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { FileService } from '../../services/file.service';
 
 @Component({
   selector: 'app-home',
@@ -9,9 +9,46 @@ import { Router } from '@angular/router';
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
-  constructor(private router: Router) {}
+  uploadProgress: number = 0;
+  isUploading: boolean = false;
+  
+  constructor(private fileService: FileService) {}
 
-  navigateToUpload() {
-    this.router.navigate(['/save']);
+  onFileSelected(event: Event): void {
+    const element = event.target as HTMLInputElement;
+    const file = element.files?.[0];
+    
+    if (file && this.isExcelFile(file)) {
+      this.uploadFile(file);
+    } else {
+      alert('Por favor, selecciona un archivo Excel válido (.xlsx, .xls)');
+    }
+  }
+
+  private uploadFile(file: File): void {
+    this.isUploading = true;
+    this.uploadProgress = 0;
+
+    this.fileService.uploadExcel(file).subscribe({
+      next: (response: any) => {
+        if (response.progress) {
+          this.uploadProgress = response.progress;
+        } else {
+          console.log('Archivo subido exitosamente:', response);
+          this.isUploading = false;
+          alert('Archivo subido con éxito');
+        }
+      },
+      error: (error) => {
+        console.error('Error al subir el archivo:', error);
+        this.isUploading = false;
+        alert('Error al subir el archivo');
+      }
+    });
+  }
+
+  private isExcelFile(file: File): boolean {
+    return file.type === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' || 
+           file.type === 'application/vnd.ms-excel';
   }
 }
