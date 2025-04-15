d2ddfdd258c8358e8e0f9881657c0cb1e4e6d874
rodrigopozodev <rodrigopozosanchez@gmail.com>
2025-04-16 00:39:17 +0200

    Arreglando url de home

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,13 +3,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { SaveComponent } from './pages/save/save.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent }, // Ruta raíz que carga el HomeComponent sin cambiar la URL
+  { path: 'home', component: HomeComponent }, // Esta ruta puede ser innecesaria, ya que la raíz ya redirige a Home
   { path: 'save', component: SaveComponent },
   {
     path: 'informes',
     loadComponent: () => import('./pages/informes/informes.component')
       .then(m => m.InformesComponent)
   },
-  { path: '**', redirectTo: 'home' }
+  { path: '**', redirectTo: '' } // Si hay una URL desconocida, redirige a la raíz
 ];
